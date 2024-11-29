fc3fbb27c4001946085d9b29c4019f1b8a934ef6
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-29 10:14:32 +0500

    idgfl

---
+++ integer2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace integer2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var K = Convert.ToInt32(Console.ReadLine());
+            var result = K / 1000;
+            Console.WriteLine(result+" kg");
+        }
+    }
+}
